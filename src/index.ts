c377fb0f2d10b1a9779273c8f7cca1c649d6a5d0
TheLDB <ldb@erikboles.com>
2023-04-11 14:12:23 -0600

    add more videos

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,29 @@ import fastify from "fastify";
 
 const server = fastify({ logger: true });
 
-const images: {title: string; image: string;}[] = [
+const images: { title: string; image: string }[] = [
   {
     title: "banner_s.png",
     image: "https://framerusercontent.com/images/A4ATGSFzLdUkscJGEJAl1xoRg.png",
   },
   {
     title: "banner_a.png",
-    image: "https://framerusercontent.com/images/amo7e1yQz0pEcRbaZFObkTpQIjc.png",
+    image:
+      "https://framerusercontent.com/images/amo7e1yQz0pEcRbaZFObkTpQIjc.png",
   },
   {
     title: "banner_g.png",
-    image: "https://framerusercontent.com/images/3HszPak0gJ3FySgT9VG9uk02j4.png",
+    image:
+      "https://framerusercontent.com/images/3HszPak0gJ3FySgT9VG9uk02j4.png",
   },
   {
     title: "banner_e.png",
-    image: "https://framerusercontent.com/images/kzNIFbdmcPta67CoMrd1x93h9o0.png",
+    image:
+      "https://framerusercontent.com/images/kzNIFbdmcPta67CoMrd1x93h9o0.png",
   },
 ];
 
-const upcomingEvents: {date: string; event: string; rsvp: string;}[] = [
+const upcomingEvents: { date: string; event: string; rsvp: string }[] = [
   {
     date: "april 11",
     event: "ideas 101 w/ farza",
@@ -40,27 +43,35 @@ const upcomingEvents: {date: string; event: string; rsvp: string;}[] = [
   {
     date: "forever",
     event: "subscribe to cal",
-    rsvp: "https://calendar.google.com/calendar/u/0?cid=Y181Y2NlMjZiOWM4ZTkyMjUxMWNiNTE4OGJmMDYxMWQwZDE3YjQyOGNiZTRmMTY0NDIyMzFlMjZjNjQwYTI4NjlmQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20"
-  }
+    rsvp: "https://calendar.google.com/calendar/u/0?cid=Y181Y2NlMjZiOWM4ZTkyMjUxMWNiNTE4OGJmMDYxMWQwZDE3YjQyOGNiZTRmMTY0NDIyMzFlMjZjNjQwYTI4NjlmQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20",
+  },
 ];
 
-const videos: {title: string; link: string}[] = [
-    {
-        title: "kickoff_apr_8.mp4",
-        link: "https://www.twitch.tv/videos/1788278186?t=00h21m05s"
-    },
-    {
-        title: "slides",
-        link: "https://www.figma.com/proto/NsoRBkV2uMDAH3X967cKB5/s3----kickoff?node-id=1-2"
-    }
+const videos: { title: string; link: string }[] = [
+  {
+    title: "ideas_101.mp4",
+    link: "https://www.twitch.tv/videos/1790897122",
+  },
+  {
+    title: "ideas_template",
+    link: "https://www.figma.com/community/file/1227500540936613761/s3-ideas-wksp",
+  },
+  {
+    title: "kickoff_apr_8.mp4",
+    link: "https://www.twitch.tv/videos/1788278186?t=00h21m05s",
+  },
+  {
+    title: "slides",
+    link: "https://www.figma.com/proto/NsoRBkV2uMDAH3X967cKB5/s3----kickoff?node-id=1-2",
+  },
 ];
 
 const enableImages = true;
 const enableVideos = true;
 
 server.get("/", async (_, reply) => {
-    reply.send("gm builder")
-})
+  reply.send("gm builder");
+});
 
 server.get("/api/imagesEnabled", async (_, reply) => {
   reply.send(enableImages);
@@ -75,12 +86,12 @@ server.get("/api/events", async (_, reply) => {
 });
 
 server.get("/api/videosEnabled", async (_, reply) => {
-    reply.send(enableVideos);
-})
+  reply.send(enableVideos);
+});
 
 server.get("/api/videos", async (_, reply) => {
-    reply.send(videos);
-})
+  reply.send(videos);
+});
 
 const start = async () => {
   try {
